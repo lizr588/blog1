63b7e77ba389062331a7d08d3f1a0cac89ca19e8
Liz R <lizr588@gmail.com>
2018-07-11 10:58:12 -0500

    blog2

--- Gemfile
+++ Gemfile
@@ -13,6 +13,9 @@ gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem 'devise'
+gem 'gravatarify', '~> 3.0.0'
+gem 'carrierwave', '~> 1.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,11 +10,13 @@ class CommentsController < ApplicationController
   # GET /comments/1
   # GET /comments/1.json
   def show
+      @post = Post.new
   end
 
   # GET /comments/new
   def new
     @comment = Comment.new
+    @post = Post.new
   end
 
   # GET /comments/1/edit
@@ -70,6 +72,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:author, :comment_entry, :post_id)
+      params.require(:comment).permit(:author, :comment_entry, :post_id, :user_id)
     end
 end
